d217f3ea0ed30983586c2536178c475946aa130d
Manuel Uberti <manuel@7bridges.eu>
2019-02-13 09:07:44 +0100

    Handle edge properties

--- src/clj_graph/graph.clj
+++ src/clj_graph/graph.clj
@@ -9,31 +9,36 @@
     (assoc g k {:value v :in #{} :out #{}})))
 
 (defn add-edge-in
-  [g k1 k2]
-  (update-in g [k1 :in] conj k2))
+  [g k1 k2 properties]
+  (update-in g [k1 :in] conj {k2 properties}))
 
 (defn add-edge-out
-  [g k1 k2]
-  (update-in g [k1 :out] conj k2))
+  [g k1 k2 properties]
+  (update-in g [k1 :out] conj {k2 properties}))
 
 (defn add-edge
   "Add an edge in from the vertex `to` to the vertex `from` in the graph `g`.
-  Add an edge out from the vertex `from` to the vertex `to` in the graph `g`."
-  [g from to]
-  (-> g
-      (add-edge-in to from)
-      (add-edge-out from to)))
+  Add an edge out from the vertex `from` to the vertex `to` in the graph `g`.
+  Add edge `properties` or empty map if `properties` is not passed."
+  ([g from to]
+   (add-edge g from to {}))
+  ([g from to properties]
+   (-> g
+       (add-edge-in to from properties)
+       (add-edge-out from to properties))))
 
 (defn remove-edge-in
   [g k1 k2]
   (let [k1-ins (get-in g [k1 :in])]
-    (->> (s/difference k1-ins #{k2})
+    (->> (remove (fn [m] (= (first (keys m)) k2)) k1-ins)
+         (into #{})
          (assoc-in g [k1 :in]))))
 
 (defn remove-edge-out
   [g k1 k2]
   (let [k1-ins (get-in g [k1 :out])]
-    (->> (s/difference k1-ins #{k2})
+    (->> (remove (fn [m] (= (first (keys m)) k2)) k1-ins)
+         (into #{})
          (assoc-in g [k1 :out]))))
 
 (defn remove-edge
@@ -54,25 +59,29 @@
 (defn neighbors
   "Get all the nodes with an edge to `k` from the graph `g`."
   [g k]
-  (s/union (in-edges g k)
-           (out-edges g k)))
+  (s/union (in-edges g k) (out-edges g k)))
 
 (defn adjacent?
   "Check there is an edge between `k1` and `k2`."
   [g k1 k2]
-  (true? (some #(= % k2) (neighbors g k1))))
+  (->> (neighbors g k1)
+       (filter (fn [m] (= (first (keys m)) k2)))
+       empty?
+       not))
 
 (defn remove-from-ins
   [g ins k]
   (reduce (fn [acc el]
-            (remove-edge acc el k))
+            (let [elk (first (keys el))]
+              (remove-edge acc elk k)))
           g
           ins))
 
 (defn remove-from-outs
   [g outs k]
   (reduce (fn [acc el]
-            (remove-edge acc k el))
+            (let [elk (first (keys el))]
+              (remove-edge acc k elk)))
           g
           outs))
 
@@ -95,3 +104,18 @@
   "Set the value of the vertex `k` in the graph `g`."
   [g k v]
   (assoc-in g [k :value] v))
+
+(defn get-edge-properties
+  "Get the properties of the edge between `k1` and `k2` in the graph `g`."
+  [g k1 k2]
+  (let [k1-outs (get-in g [k1 :out])]
+    (-> (filter (fn [m] (= (first (keys m)) k2)) k1-outs)
+        first
+        (get k2))))
+
+(defn set-edge-properties
+  "Update the properties of the edge between `k1` and `k2` in the graph `g`."
+  [g k1 k2 properties]
+  (-> g
+      (remove-edge k1 k2)
+      (add-edge k1 k2 properties)))
--- test/clj_graph/graph_test.clj
+++ test/clj_graph/graph_test.clj
@@ -2,10 +2,10 @@
   (:require [clj-graph.graph :as cg]
             [clojure.test :refer :all]))
 
-(def g-test {:a {:value "1" :in #{} :out #{:b}}
-             :b {:value "2" :in #{:a} :out #{:c}}
-             :c {:value "3" :in #{:b} :out #{:d}}
-             :d {:value "4" :in #{:c} :out #{}}})
+(def g-test {:a {:value "1" :in #{} :out #{{:b {:p1 :test}}}}
+             :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+             :c {:value "3" :in #{{:b {:p1 :test}}} :out #{{:d {:p1 :test}}}}
+             :d {:value "4" :in #{{:c {:p1 :test}}} :out #{}}})
 
 (deftest adjacent?-test
   (testing "Testing adjacent? function"
@@ -14,39 +14,46 @@
 
 (deftest neighbors-test
   (testing "Testing neighbors function"
-    (is (= (cg/neighbors g-test :c) #{:b :d}))))
+    (is (= (cg/neighbors g-test :c) #{{:b {:p1 :test}} {:d {:p1 :test}}}))))
 
 (deftest add-vertex-test
   (testing "Testing add-vertex function"
     (is (= (cg/add-vertex g-test :e "5")
-           {:a {:value "1" :in #{} :out #{:b}}
-            :b {:value "2" :in #{:a} :out #{:c}}
-            :c {:value "3" :in #{:b} :out #{:d}}
-            :d {:value "4" :in #{:c} :out #{}}
+           {:a {:value "1" :in #{} :out #{{:b {:p1 :test}}}}
+            :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+            :c {:value "3" :in #{{:b {:p1 :test}}} :out #{{:d {:p1 :test}}}}
+            :d {:value "4" :in #{{:c {:p1 :test}}} :out #{}}
             :e {:value "5" :in #{} :out #{}}}))))
 
 (deftest remove-vertex-test
   (testing "Testing remove-vertex function"
     (is (= (cg/remove-vertex g-test :d)
-           {:a {:value "1" :in #{} :out #{:b}}
-            :b {:value "2" :in #{:a} :out #{:c}}
-            :c {:value "3" :in #{:b} :out #{}}}))))
+           {:a {:value "1" :in #{} :out #{{:b {:p1 :test}}}}
+            :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+            :c {:value "3" :in #{{:b {:p1 :test}}} :out #{}}}))))
 
 (deftest add-edge-test
   (testing "Testing add-edge function"
-    (is (= (cg/add-edge g-test :d :a)
-           {:a {:value "1" :in #{:d} :out #{:b}}
-            :b {:value "2" :in #{:a} :out #{:c}}
-            :c {:value "3" :in #{:b} :out #{:d}}
-            :d {:value "4" :in #{:c} :out #{:a}}}))))
+    (is
+     (= (cg/add-edge g-test :d :a)
+        {:a {:value "1" :in #{{:d {}}} :out #{{:b {:p1 :test}}}}
+         :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+         :c {:value "3" :in #{{:b {:p1 :test}}} :out #{{:d {:p1 :test}}}}
+         :d {:value "4" :in #{{:c {:p1 :test}}} :out #{{:a {}}}}}))
+    (is
+     (= (cg/add-edge g-test :d :a {:p1 :test})
+        {:a {:value "1" :in #{{:d {:p1 :test}}} :out #{{:b {:p1 :test}}}}
+         :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+         :c {:value "3" :in #{{:b {:p1 :test}}} :out #{{:d {:p1 :test}}}}
+         :d {:value "4" :in #{{:c {:p1 :test}}} :out #{{:a {:p1 :test}}}}}))))
 
 (deftest remove-edge-test
   (testing "Testing remove-edge function"
     (is (= (cg/remove-edge g-test :b :c)
-           {:a {:value "1" :in #{} :out #{:b}}
-            :b {:value "2" :in #{:a} :out #{}}
-            :c {:value "3" :in #{} :out #{:d}}
-            :d {:value "4" :in #{:c} :out #{}}}))))
+           {:a {:value "1" :in #{} :out #{{:b {:p1 :test}}}}
+            :b {:value "2" :in #{{:a {:p1 :test}}} :out #{}}
+            :c {:value "3" :in #{} :out #{{:d {:p1 :test}}}}
+            :d {:value "4" :in #{{:c {:p1 :test}}} :out #{}}}))))
 
 (deftest get-vertex-value
   (testing "Testing get-vertex-value function"
@@ -55,7 +62,7 @@
 (deftest set-vertex-value
   (testing "Testing set-vertex-value function"
     (is (= (cg/set-vertex-value g-test :a "0")
-           {:a {:value "0" :in #{} :out #{:b}}
-            :b {:value "2" :in #{:a} :out #{:c}}
-            :c {:value "3" :in #{:b} :out #{:d}}
-            :d {:value "4" :in #{:c} :out #{}}}))))
+           {:a {:value "0" :in #{} :out #{{:b {:p1 :test}}}}
+            :b {:value "2" :in #{{:a {:p1 :test}}} :out #{{:c {:p1 :test}}}}
+            :c {:value "3" :in #{{:b {:p1 :test}}} :out #{{:d {:p1 :test}}}}
+            :d {:value "4" :in #{{:c {:p1 :test}}} :out #{}}}))))
